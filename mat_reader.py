28027a61aea5bb08b46bc34037271bd29ae380c8
gopiprasanthpotipireddy <gopi.prasanth007@gmail.com>
2018-07-18 10:47:16 +0530

    converted 15 (256,256) images into (512,512)

--- mat_reader.py
+++ mat_reader.py
@@ -40,7 +40,7 @@ columns =['PID', 'image', 'label', 'tumorBorder', 'tumorMask']
 
 d={'PID':[], 'image':[], 'label':[], 'tumorBorder':[], 'tumorMask':[]}
 
-for i in range(1000):
+for i in range(3064):
     d['PID'].append(p[i].PID)
     d['image'].append(p[i].image)
     d['label'].append(p[i].label)
@@ -53,41 +53,39 @@ Patient_data=pd.DataFrame(list(d.values()),columns)
 
 Patient_data=Patient_data.transpose()
 
-#preprocessing image datafor training x_train
+#preprocessing image data for training x_train
 
-
-#files with correpted images
+#files with corrupted images from (256,256) to (512,512)
 count=0
 for i in range(3064): 
      if np.array(p[i].image).shape[0]!= 512 or np.array(p[i].image).shape[1]!=512:
-         print("worst")
+         #print("worst")
+         p[i].image=np.concatenate((np.zeros((256,256)),np.zeros((256,256))),axis=0)
+         p[i].image=np.concatenate((p[i].image,np.zeros((512,256))),axis=1)
          print(i)
          count+=1
 
-#training images only having (512,512)
+""" training images only having (512,512)"""
 image_train=[]
 label_train=[]
          
-for i in range(1207,3064):
+
+for i in range(3064):
     image_train.append(p[i].image)
     label_train.append(p[i].label)
+    
+image_train=np.array(image_train)
+label_train=np.array(label_train)
 
-
-
-x_train=np.array(image_train)
+"""#imaage train data for x_train
+image_train=np.array(d['image'])
+#label for y_train
 label_train=np.array(d['label'])
-np.concatenate(x_train,np.array(image_train))
-
-np.mat(d['image']).shape
-
+"""
 #feature reduction  formula X = X.reshape(X.shape[0]*X.shape[1]*X.shape[2],X.shape[3]).T
-
-
 
 image_train= image_train.reshape(image_train.shape[0],image_train.shape[1]*image_train.shape[2])
 
-#label for y_train
-label_train=np.array(d['label'])
 label_train=label_train.reshape(label_train.shape[0],)
 
 image_train, label_train = shuffle(image_train, label_train, random_state=42)
@@ -97,17 +95,17 @@ Tumour_classifier = tree.DecisionTreeClassifier()
 #testing the model
 
 
-X_train, X_test, y_train, y_test = train_test_split(image_train, label_train, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(image_train, label_train, test_size=0.3, random_state=42)
 Tumour_classifier.fit(X_train, y_train)
 
 preds = Tumour_classifier.predict(X_test)
 
-
-print("Accuracy:", accuracy_score(y_test,preds))
+accuracy=accuracy_score(y_test,preds)
+print("Accuracy:", accuracy)
 
 
 #image plotting from (512,512) matrix
-plt.imshow(image_train[1])
+#plt.imshow(image_train[1])
 
 
 
